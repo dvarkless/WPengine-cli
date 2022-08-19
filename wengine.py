a186cd87b22e60a4b494e7d25fbf6910f9dabad2
dvarkless <amir911157@yandex.ru>
2022-08-19 19:10:08 +0300

    Refactor: move argument parsing to __init__ method

--- wengine.py
+++ wengine.py
@@ -19,131 +19,98 @@ class Plugin:
         self.wp_changer = WallpaperChanger(self.steamdir, self.config_path)
         self.settings_changer = SettingsChanger(
             self.steamdir, self.config_path)
-
+        
+        # defining argument parser:
         parser = argparse.ArgumentParser(
-            description='CLI addon for Wallpaper Engine KDE tool',
-            usage='''wengine <command> [<args>]
+            description='CLI addon for Wallpaper Engine KDE tool')
+        subparsers = parser.add_subparsers(dest='command')
+        wallpaper_parser = subparsers.add_parser('wallpaper')
+        wallpaper_parser.set_defaults(func=self.wallpaper)
 
-        Commands:
-           wallpaper - Operations with wallpaper
-           settings  - Change settings in Wallpaper Engine
-           update    - Update list of installed wallpapers
-           config    - Change specified configs 
-           pull      - Pull data about current wallpaper and SteamLibrary from
-                       Wallpaper Engine
-           undo      - Undo last change, useful then you get a black screen
-        ''')
-        self.parser_0 = parser
-        parser.add_argument('command', help='Subcommand to run')
+        wallpaper_subparsers = wallpaper_parser.add_subparsers(dest='wallpaper_command')
+        setup_wallpaper_parser = wallpaper_subparsers.add_parser('setup')
+        setup_wallpaper_parser.add_argument(
+            '--strict', help='disable fuzzy finder then searching by title', action=argparse.BooleanOptionalAction, default='False')
+        setup_wallpaper_parser.add_argument('name_or_id', help='Name or ID') 
+        random_wallpaper_parser = wallpaper_subparsers.add_parser('random')
+        random_wallpaper_parser.add_argument(
+            '--type', help='Type of wallpapers to choose from. Syntax: "--type scene,video,web"', choices=['scene', 'video', 'web'])
+        random_wallpaper_parser.add_argument(
+            '--contentrating', help='Filter out NSFW wallpapers and vice versa. Syntax: "--contentrating Everyone,Questionable,Mature" (Note! starts with capital letter)', choices=['Everyone', 'Questionable', 'Mature'])
+        random_wallpaper_parser.add_argument(
+            '--tags', help='Filter by tags specified in wallpaper description. Syntax: "--tags Nature,Anime,Game"')
+        random_wallpaper_parser.add_argument(
+            '--nsfw', help='Add wallpapers with rating "Mature" into the mix', action=argparse.BooleanOptionalAction, default='False')
 
-        # parser.add_argument('--random', action=argparse.BooleanOptionalAction,
-        #                     help='Setup random wallpaper from WallpaperEngine directory', default=False)
-        # parser.add_argument('--undo', action=argparse.BooleanOptionalAction,
-        #                     help='Return previous wallpaper. Useful then you got a black screen', default=False)
-        # parser.add_argument('--version', action='version', version='%(prog)s 0.1')
-        # parser.add_argument('--steamdir', type=Path,
-        #                     help='Specify your Steam directory with this argument')
-        # parser.add_argument('--verbose', action=argparse.BooleanOptionalAction,
-        #                     help='Debug utility', default=False)
+        settings_parser = subparsers.add_parser('settings')
+        settings_parser.set_defaults(func=self.settings)
+        settings_subparsers = settings_parser.add_subparsers(dest='settings_command')
 
-        args = parser.parse_args(sys.argv[1:2])
-        if not hasattr(self, args.command):
-            self.help()
-        getattr(self, args.command)()
+        setup_settings_parser = settings_subparsers.add_parser('setup')
+        name_setup_settings_parser = setup_settings_parser.add_subparsers(dest='subcommand').add_parser('name')
+        name_setup_settings_parser.add_argument('value')
 
-    def config(self):
-        parser = argparse.ArgumentParser(
-            description='Operations with wallpaper',
-            usage='''wengine config <command> <flags>
+        get_settings_parser = settings_subparsers.add_parser('get')
+        get_settings_parser.add_argument('name', help='setting name')
 
-        Commands:
-            setup  - setup config
-            get    - get config
+        update_parser = subparsers.add_parser('update')
+        update_parser.set_defaults(func=self.update)
 
-        ''')
+        pull_parser = subparsers.add_parser('pull')
+        pull_parser.set_defaults(func=self.pull)
 
-        parser.add_argument('command', help='Subcommand to run')
-        args = parser.parse_args(sys.argv[2:3])
-        if args.command == 'setup':
+        undo_parser = subparsers.add_parser('pull')
+        undo_parser.set_defaults(func=self.undo)
+
+        parser.add_argument('--version', action='version', version='%(prog)s 0.1.123')
+        parser.add_argument('--verbose', action=argparse.BooleanOptionalAction,
+                            help='Debug utility', default=False)
+
+        args = parser.parse_args()
+        dict_args = vars(args).copy()
+        dict_args.pop('func')
+        dict_args.pop('command')
+        args.func(**dict_args)
+
+    def config(self, **kwargs):
+        if kwargs['command'] == 'setup':
             pass
-        elif args.command == 'get':
+        elif kwargs['command'] == 'get':
             pass
-        else:
-            self.help(parser)
 
-    def pull(self):
+    def pull(self, **kwargs):
         settings_list = self.settings_changer.read()
         for setting_tuple in settings_list:
             self.handler.add_pos(*setting_tuple)
 
-    def wallpaper(self):
-        parser = argparse.ArgumentParser(
-            description='Operations with wallpaper')
-        """
-            usage='''wengine wallpaper <command> <flags>
-
-        Commands:
-            setup  - setup wallpaper by id or name
-            random - setup random wallpaper. You can specify filters with
-                     arguments --type, --contentrating, --tags.
-                     (for example choose random wallpaper from nature scenes:
-                         ... random --type scenes --tags Nature)
-            name   - get current wallpaper name and id
-                     in format: <id> "name".
-            accent - get wallpaper's accent color in RGB uint8 format:
-                     R G B
-            get    - get info about the wallpaper from its config
-
-        ''')
-        """
-        parser.add_argument('command', help='Subcommand to run')
-
-        parser.add_argument(
-            '--type', help='random: Type of wallpapers to choose from. Syntax: "--type scene,video,web"', choices=['scene', 'video', 'web'])
-        parser.add_argument(
-            '--contentrating', help='random: Filter out NSFW wallpapers and vice versa. Syntax: "--contentrating Everyone,Mature" (Note! starts with capital letter)', choices=['Everyone', 'Questionable', 'Mature'])
-        parser.add_argument(
-            '--tags', help='random: Filter by tags specified in wallpaper description. Syntax: "--tags Nature,Anime,Game"')
-        parser.add_argument(
-            '--strict', help='random: disable fuzzy finder then searching by title', action=argparse.BooleanOptionalAction, default='False')
-
-        parser.add_argument(
-            '--nsfw', help='random: add wallpapers with rating "Mature" into the mix', action=argparse.BooleanOptionalAction, default='False')
-        args = parser.parse_args(sys.argv[2:3])
-        if args.command == 'setup':
-            parser.add_argument(
-                'name_or_id', help='Title of the wallpaper or its id')
-            args = parser.parse_args(sys.argv[2:])
-            self.wp_changer.setup(args.name_or_id)
-        elif args.command == 'random':
+    def wallpaper(self, **kwargs):
+        if kwargs['wallpaper_command'] == 'setup':
+            self.wp_changer.setup(kwargs['name_or_id'], fuzzy=not kwargs['strict'])
+        elif kwargs['wallpaper_command'] == 'random':
             filters = {}
-            args = parser.parse_args(sys.argv[2:])
-            if hasattr(args, 'type'):
-                if args.type:
-                    filters['type'] = args.type.split(',')
-            if hasattr(args, 'contentrating'):
-                if args.contentrating:
-                    filters['contentrating'] = args.contentrating.split(',')
-            if hasattr(args, 'tags'):
-                if args.tags:
-                    filters['tags'] = args.tags.split(',')
-            if args.nsfw:
-                if 'contentrating' not in filters.keys():
-                    filters['contentrating'] = ['Everyone', 'Unspecified', 'Questionable']
-            self.wp_changer.setup_random(filters=filters, fuzzy=not args.strict)
+            if kwargs['type']:
+                filters['type'] = kwargs['type'].split()
+            if kwargs['contentrating']:
+                filters['contentrating'] = kwargs['contentrating'].split()
+            elif kwargs['nsfw']:
+                filters['contentrating'] = ['Everyone', 'Unspecified', 'Questionable', 'Mature']
+            if kwargs['tags']:
+                filters['tags'] = kwargs['tags'].split()
 
-        elif args.command == 'name':
+            self.wp_changer.setup_random(filters=filters)
+
+        elif kwargs['wallpaper_command'] == 'name':
             id, name = self.wp_changer.get_last_id_name()
             print(f'<{id}> "{name}"')
 
-        elif args.command == 'accent':
+        elif kwargs['wallpaper_command'] == 'accent':
             id, _ = self.wp_changer.get_last_id_name()
             wp_data = self.handler.get_data(id)
             rgb_str = wp_data['general']['properties']['schemecolor']['value']
             rgb_vals = [int(float(val)*255) for val in rgb_str.split()]
             print("{} {} {}".format(*rgb_vals))
 
-        elif args.command == 'get':
+        elif kwargs['wallpaper_command'] == 'get':
             id, _ = self.wp_changer.get_last_id_name()
             wp_data = self.handler.get_data(id)
 
@@ -154,53 +121,23 @@ class Plugin:
                     else:
                         print(f'{"  "*level} {name}="{val}"')
             recursion_dict_printer(wp_data)
-        else:
-            self.help(parser)
 
-    def undo(self):
+    def undo(self, **kwargs):
         undoable_settings = self.settings_changer.settings_list
-        for setting in undoable_settings:
-            if setting in ("SteamLibraryPath"):
+        for name in undoable_settings:
+            if name == "SteamLibraryPath":
                 continue
-            val = self.handler.get_data(setting)
-            self.settings_changer.setup(setting, val)
+            val = self.handler.get_data(name)
+            self.settings_changer.setup(name, val)
 
-    def update(self):
+    def update(self, **kwargs):
         self.wp_changer.get_all_data()
 
-    def settings(self):
-        parser = argparse.ArgumentParser(
-            description='Change settings for Wallpaper Engine',
-            usage='''wengine settings <command> <flags>
-
-        Commands:
-            setup - setup specific setting for Wallpaper Engine
-            get   - get specific setting
-        ''')
-
-        parser.add_argument('command', help='Subcommand to run')
-        parser.add_argument(
-            'setting_name', help='Name of the setting to change or print')
-        args = parser.parse_args(sys.argv[2:4])
-        if args.command == 'setup':
-            parser.add_argument('setting_val', help='Subcommand to run')
-            args = parser.parse_args(sys.argv[2:5])
-
-            setting = args.setting_name
-            val = args.setting_val
-            self.settings_changer.setup(setting, val)
-        elif args.command == 'get':
-            setting = args.setting_name
-            print(self.settings_changer.read(setting))
-        else:
-            self.help(parser)
-
-    def help(self, parser=None, /):
-        if parser is None:
-            parser = self.parser_0
-        print('Unrecognized command')
-        parser.print_help()
-        exit(1)
+    def settings(self, **kwargs):
+        if kwargs['settings_command'] == 'setup':
+            self.settings_changer.setup(kwargs['subcommand'], kwargs['val'])
+        elif kwargs['settings_command'] == 'get':
+            print(self.settings_changer.read(kwargs['subcommand']))
 
 
 if __name__ == "__main__":
